7a281547779897af2aad4b80048d5437f345c357
subatomic450 <165378970+subatomic450@users.noreply.github.com>
2024-11-23 14:53:57 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : 'YOUR-DATABASE-ENDPOINT.ap-south-1.rds.amazonaws.com',
+    DB_HOST : 'three-tier-database.cf2euwqoggvb.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
-    DB_PWD : 'YourPassword',
+    DB_PWD : 'Iam123456',
     DB_DATABASE : 'webappdb'
 });
